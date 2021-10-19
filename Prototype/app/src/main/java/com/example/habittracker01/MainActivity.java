2b37323b69921993d84a81963e72088c9b12fe54
Aaron Skiba <askiba@ualberta.ca>
2021-10-18 18:35:32 -0600

    Updated project to ListyCity Lab 3 Skeleton

--- Prototype/app/src/main/java/com/example/habittracker01/MainActivity.java
+++ Prototype/app/src/main/java/com/example/habittracker01/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.habittracker01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare the variables so that you will be able to reference it later.
+    ListView cityList;
+    ArrayAdapter<Habit> cityAdapter;
+    ArrayList<Habit> cityDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Vancouver", "Toronto", "Hamilton", "Denver", "Los Angeles"};
+        String []provinces = {"AB", "BC", "ON", "ON", "CO", "CA"};
+
+        cityDataList = new ArrayList<>();
+
+        for (int i = 0; i < cities.length; i++) {
+            cityDataList.add(new Habit(cities[i], provinces[i]));
+        }
+
+        cityAdapter = new CustomList(this, cityDataList);
+
+        cityList.setAdapter(cityAdapter);
+
     }
+
+
 }
