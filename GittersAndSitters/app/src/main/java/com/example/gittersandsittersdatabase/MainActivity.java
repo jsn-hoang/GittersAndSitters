1dcd8745440ff67519654d5efb4256a750899b64
Mark McGoey <mmcgoey@ualberta.ca>
2021-10-28 16:55:12 -0600

    worked on UI for login and register. Also worked on Main

--- GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/MainActivity.java
+++ GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/MainActivity.java
@@ -2,21 +2,38 @@ package com.example.gittersandsittersdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     // Declare variables to be referenced
-    private FirebaseAuth mAuth;             // The entry point of the Firebase Authentication SDK
+    //private FirebaseAuth mAuth;             // The entry point of the Firebase Authentication SDK
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();     // Get an instance of Firebase Authentication SDK
+
+        register = findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        //mAuth = FirebaseAuth.getInstance();     // Get an instance of Firebase Authentication SDK
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this,Register.class));
+                break;
+        }
     }
 }
