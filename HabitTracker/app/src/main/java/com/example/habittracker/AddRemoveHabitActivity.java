7ebfd2e2417862a0547a3b6b647c37829f8aed26
Aaron Skiba <askiba@ualberta.ca>
2021-10-28 23:51:09 -0600

    Create AddRemoveHabitActivity.java
    Added AddRemoveHabitActivity

---
+++ HabitTracker/app/src/main/java/com/example/habittracker/AddRemoveHabitActivity.java
@@ -0,0 +1,15 @@
+package com.example.habittracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddRemoveHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_remove_habit);
+
+    }
+}
