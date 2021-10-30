52872258cb9a02c1364d0084f1f8a16afadae790
Mark McGoey <mmcgoey@ualberta.ca>
2021-10-29 19:55:22 -0600

    made changes in ProfileActivity class and xml

--- GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/MainActivity.java
+++ GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     if(user.isEmailVerified()){
                         // redirect to user profile
-                        startActivity(new Intent(MainActivity.this,Logout.class));
+                        startActivity(new Intent(MainActivity.this,ProfileActivity.class));
                     }else{
                         user.sendEmailVerification();
                         Toast.makeText(MainActivity.this, "Check your email to verify your account !", Toast.LENGTH_LONG).show();
--- GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/ProfileActivity.java
+++ GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/ProfileActivity.java
@@ -5,10 +5,19 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * This class is responsible for logging out of the HabitTracker app.
@@ -16,6 +25,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class ProfileActivity extends AppCompatActivity {
 
     // Declare variables to be referenced
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
     private Button logout;
 
     @Override
@@ -36,12 +49,33 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
         user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
         userID = user.getUid();
 
         final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
         final TextView userNameTextView = (TextView) findViewById(R.id.userName);
         final TextView emailTextView = (TextView) findViewById(R.id.emailAddress);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String username = userProfile.username;
+                    String email = userProfile.email;
+
+                    greetingTextView.setText("Welcome," + username);
+                    userNameTextView.setText(username);
+                    emailTextView.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this,"Error",Toast.LENGTH_LONG).show();
+            }
+        });
         
     }
 }
