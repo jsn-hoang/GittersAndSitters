e32a968ba17ea34d2efeca21bbddf294fa1ec7ae
Mark McGoey <mmcgoey@ualberta.ca>
2021-11-03 12:33:23 -0600

    added snapshot stuff so the app gets info from firestore

--- GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/ProfileActivity.java
+++ GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.gittersandsittersdatabase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +21,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * This class is responsible for logging out of the HabitTracker app.
@@ -25,9 +31,9 @@ import com.google.firebase.database.ValueEventListener;
 public class ProfileActivity extends AppCompatActivity {
 
     // Declare variables to be referenced
-    private FirebaseUser user;
-    private DatabaseReference reference;
     private String userID;
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
 
     private Button logout;
 
@@ -36,6 +42,9 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+
+        fStore = FirebaseFirestore.getInstance();
 
         logout = findViewById(R.id.logout);
 
@@ -48,34 +57,39 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Users");
-        userID = user.getUid();
 
+        userID = mAuth.getCurrentUser().getUid();
+        //userID = user.getUid();
+
+        DocumentReference docRef = fStore.collection("Users").document(userID);
         final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
         final TextView userNameTextView = (TextView) findViewById(R.id.userName);
         final TextView emailTextView = (TextView) findViewById(R.id.emailAddress);
 
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userProfile = snapshot.getValue(User.class);
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String username = (String) document.getData().get("userName");
+                        String email = (String) document.getData().get("email");
 
-                if(userProfile != null){
-                    String username = userProfile.getUsername();
-                    String email = userProfile.getEmail();
+                        greetingTextView.setText("Welcome, " + username);
+                        userNameTextView.setText(username);
+                        emailTextView.setText(email);
 
-                    greetingTextView.setText("Welcome, " + username);
-                    userNameTextView.setText(username);
-                    emailTextView.setText(email);
+
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
                 }
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(ProfileActivity.this,"Error",Toast.LENGTH_LONG).show();
-            }
         });
-        
+
     }
 }
