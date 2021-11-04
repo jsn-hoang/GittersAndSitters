378c051d575af68a63d3cbb6014ca3873d53ec6c
Aaron Skiba <askiba@ualberta.ca>
2021-11-04 14:23:02 -0600

    Making Progress on Intent Testing
    Had to add ids to xml files

--- GittersAndSitters/app/src/androidTest/java/com/example/gittersandsittersdatabase/HabitActivityTest.java
+++ GittersAndSitters/app/src/androidTest/java/com/example/gittersandsittersdatabase/HabitActivityTest.java
@@ -3,13 +3,20 @@ package com.example.gittersandsittersdatabase;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.test.espresso.Espresso;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,22 +26,56 @@ public class HabitActivityTest {
 
     @Rule
     // ActivityTestRule provides functional testing of a single activity.
-    public ActivityTestRule<HabitActivity> HabitActivityActivityTestRule = new ActivityTestRule<>(HabitActivity.class, true, false);
+    // Rule will be launched before each test
+    public ActivityTestRule<HabitActivity> HabitActivityTestRule =
+            new ActivityTestRule<HabitActivity>(HabitActivity.class, true, true) {
 
-    private String email = "askiba@ualberta.ca";
-    private String password = "123456";
+        @Override
+        protected Intent getActivityIntent() {
+        Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        // Create and put a User object before
+        User user = new User("testUsername","testEmail");
+        Intent result = new Intent(targetContext, HabitActivity.class);
+        result.putExtra("user", user);
+        return result;
+        }
+    };
 
 
     @Before
     public void setUp() throws Exception {
     }
 
+    // Test functionality of the floating action button
     @Test
-    public void testUserInputScenario() {
-        //input some text in the email field
-        Espresso.onView(withId(R.id.login_emailAddress)).perform(typeText(email));
-        Espresso.onView(withId(R.id.login_password)).perform(typeText(password), closeSoftKeyboard());
-        Espresso.onView(withId(R.id.login_button)).perform(click());
+    public void testFloatingActionButton() {
+        // Click the floating action button
+        Espresso.onView(withId(R.id.add_habit_FAB)).perform(click());
+        // Check that the activity has changed to AddRemoveHabitActivity
+        Espresso.onView(withId(R.id.activity_add_remove_habit_id)).check(matches(isDisplayed()));
+    }
+
+    // Test functionality of the "Today Habits" and "All Habits" tabs
+    @Test
+    public void testSwitchingTodayHabitsAndAllHabits() {
+        // Click all Habits
+        Espresso.onView(withId(R.id.all_habits_tab)).perform(click());
+        // Check that the Activity is in all habits mode
+        // TODO
+        // Click today Habits
+        Espresso.onView(withId(R.id.todays_habits_tab)).perform(click());
+        // Check that the Activity is in all habits mode
+        // TODO
+    }
+
+    // Test functionality of the logout button
+    @Test
+    public void testLogoutButton() {
+        // Click logout button
+        Espresso.onView(withId(R.id.logout_button)).perform(click());
+        // Test if Activity switched to LogoutActivity
+        Espresso.onView(withId(R.id.activity_logout_id)).check(matches(isDisplayed()));
+
     }
 
     @After
