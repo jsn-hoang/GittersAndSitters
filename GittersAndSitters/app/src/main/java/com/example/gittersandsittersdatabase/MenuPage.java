9ee9c63cc7e168a45f61d1bb3f8830b01ca9c7e2
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-11-27 20:47:13 -0700

    Update MenuPage.java

--- GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/MenuPage.java
+++ GittersAndSitters/app/src/main/java/com/example/gittersandsittersdatabase/MenuPage.java
@@ -10,12 +10,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MenuPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MenuPage extends AppCompatActivity {
 
-    private DrawerLayout drawer;
+    // private DrawerLayout drawer;
     private User user;
 
     @Override
@@ -26,51 +28,58 @@ public class MenuPage extends AppCompatActivity implements NavigationView.OnNavi
         // Get the user intent
         user = (User) getIntent().getSerializableExtra("user");
 
-//        Toolbar toolbar = findViewById(R.id.toolBar);
-//        setSupportActionBar(toolbar);
+        final Button homeButton = findViewById(R.id.nav_home);
+        final Button eventButton = findViewById(R.id.nav_event_history);
+        final Button friendButton = findViewById(R.id.nav_friends);
+        final Button requestButton = findViewById(R.id.nav_requests);
+        final Button logOutButton = findViewById(R.id.nav_logout);
 
-//        drawer = findViewById(R.id.drawer_layout);
-//
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-//                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
 
-        //
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPage.this, HabitActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPage.this, HabitEventActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+
+        friendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPage.this, FollowFeedActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPage.this, FollowRequestActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPage.this, ProfileActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
     }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_home:
-                startActivity(new Intent(MenuPage.this, HabitActivity.class).putExtra("user", user));
-                break;
-            case R.id.nav_event_history:
-                startActivity(new Intent(MenuPage.this, HabitEventActivity.class).putExtra("user", user));
-                break;
-            case R.id.nav_friends:
-                startActivity(new Intent(MenuPage.this, FollowFeedActivity.class).putExtra("user", user));
-                break;
-            case R.id.nav_requests:
-                startActivity(new Intent(MenuPage.this, FollowRequestActivity.class).putExtra("user", user));
-                break;
-            case R.id.nav_logout:
-                startActivity(new Intent(MenuPage.this, ProfileActivity.class).putExtra("user", user));
-                break;
-        }
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START);
-        }else{
-            super.onBackPressed();
-        }
-
-    }
-
+    
 }
+      
