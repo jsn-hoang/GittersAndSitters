e7dab1e02091180bffd2a6dee44788e3b1de3388
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-11-28 20:59:26 -0700

    Create AddRemoveEventActivity.java

---
+++ GittersAndSitters/app/src/androidTest/java/com/example/gittersandsittersdatabase/AddRemoveEventActivity.java
@@ -0,0 +1,63 @@
+package com.example.gittersandsittersdatabase;
+
+import static androidx.core.content.res.TypedArrayUtils.getText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.TextView;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import static org.hamcrest.core.StringContains.containsString;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+
+public class AddRemoveEventActivityTest {
+
+    @Rule
+    // ActivityTestRule provides functional testing of a single activity.
+    // Rule will be launched before each test
+    public ActivityTestRule<AddRemoveEventActivity> AddRemoveEventActivityTestRule =
+            new ActivityTestRule<AddRemoveEventActivity>(AddRemoveEventActivity.class, true, true) {
+
+                @Override
+                protected Intent getActivityIntent() {
+                    Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+                    User user = new User("testID","testUsername","testEmail");
+                    Intent result = new Intent(targetContext, FollowingActivity.class);
+                    result.putExtra("user", user);
+
+                    return result;
+                }
+            };
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
